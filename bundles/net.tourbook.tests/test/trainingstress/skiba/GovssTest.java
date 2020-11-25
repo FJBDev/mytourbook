875f7d4939b7788417544642343d4da33d75d2fb
FJBDev <25235665+FJBDev@users.noreply.github.com>
2020-11-25 18:55:02 +0000

    Update

--- bundles/net.tourbook.tests/test/trainingstress/skiba/GovssTest.java
+++ bundles/net.tourbook.tests/test/trainingstress/skiba/GovssTest.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 
 import net.tourbook.data.TourData;
@@ -18,24 +17,22 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 
+import utils.Comparison;
 import utils.Initializer;
 
 class GovssTest {
 
-   private static TourPerson  tourPerson;
+   private static TourPerson              tourPerson;
    private static SAXParser               parser;
    private static DeviceData              deviceData;
    private static HashMap<Long, TourData> newlyImportedTours;
    private static HashMap<Long, TourData> alreadyImportedTours;
    private static GarminDeviceDataReader  deviceDataReader;
 
-   /**
-    * Resource path to GPX file, generally available from net.tourbook Plugin in test/net.tourbook
-    */
-   public static final String IMPORT_FILE_PATH = "/trainingstress/skiba/files/Move_2017_09_30_05_36_06_Trail+running-MtWhitney.gpx"; //$NON-NLS-1$
+   public static final String             IMPORT_FILE_PATH = "/trainingstress/skiba/files/Move_2017_09_30_05_36_06_Trail+running-MtWhitney.gpx"; //$NON-NLS-1$
 
    @BeforeAll
-   static void setUp() throws ParserConfigurationException, SAXException {
+   static void setUp() {
 
       tourPerson = new TourPerson();
       tourPerson.setGovssThresholdPower(367);
@@ -64,7 +61,7 @@ class GovssTest {
 
       parser.parse(gpx, gpxSaxHandler);
 
-      final TourData tour = newlyImportedTours.get(Long.valueOf(2017930123618648L));
+      final TourData tour = Comparison.RetrieveImportedTour(newlyImportedTours);
 
       final Integer govss = new Govss(tourPerson, tour).Compute();
       assert govss.equals(114);
