5055e62b9768acf7fbc02c122a620beb007fd8ad
FJBDev <25235665+FJBDev@users.noreply.github.com>
2020-11-25 18:51:44 +0000

    Migrating the GOVSS test in the tests suite

--- bundles/net.tourbook.tests/test/trainingstress/skiba/GovssTest.java
+++ bundles/net.tourbook.tests/test/trainingstress/skiba/GovssTest.java
@@ -1,38 +1,41 @@
-package net.tourbook.trainingstress;
+package trainingstress.skiba;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 
-import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 
-import net.tourbook.common.time.TimeTools;
 import net.tourbook.data.TourData;
 import net.tourbook.data.TourPerson;
-import net.tourbook.device.gpx.GPXDeviceDataReader;
+import net.tourbook.device.garmin.GarminDeviceDataReader;
 import net.tourbook.device.gpx.GPX_SAX_Handler;
 import net.tourbook.importdata.DeviceData;
+import net.tourbook.trainingstress.Govss;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 
+import utils.Initializer;
+
 class GovssTest {
 
    private static TourPerson  tourPerson;
-   private static SAXParser   parser;
+   private static SAXParser               parser;
+   private static DeviceData              deviceData;
+   private static HashMap<Long, TourData> newlyImportedTours;
+   private static HashMap<Long, TourData> alreadyImportedTours;
+   private static GarminDeviceDataReader  deviceDataReader;
 
    /**
     * Resource path to GPX file, generally available from net.tourbook Plugin in test/net.tourbook
     */
-   public static final String IMPORT_FILE_PATH = "/net/tourbook/trainingStress/files/Move_2017_09_30_05_36_06_Trail+running-MtWhitney.gpx"; //$NON-NLS-1$
+   public static final String IMPORT_FILE_PATH = "/trainingstress/skiba/files/Move_2017_09_30_05_36_06_Trail+running-MtWhitney.gpx"; //$NON-NLS-1$
 
    @BeforeAll
    static void setUp() throws ParserConfigurationException, SAXException {
-      TimeTools.setDefaultTimeZone("UTC");
 
       tourPerson = new TourPerson();
       tourPerson.setGovssThresholdPower(367);
@@ -40,22 +43,17 @@ class GovssTest {
       tourPerson.setWeight(70);
       tourPerson.setGovssTimeTrialDuration(3600);
 
-      //Xml Parser
-      final SAXParserFactory factory = SAXParserFactory.newInstance();
-      parser = factory.newSAXParser();
-      parser.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
-      parser.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+      parser = Initializer.initializeParser();
+      deviceData = new DeviceData();
+      newlyImportedTours = new HashMap<>();
+      alreadyImportedTours = new HashMap<>();
+      deviceDataReader = new GarminDeviceDataReader();
    }
 
    @Test
    void testComputeGovss() throws SAXException, IOException {
 
       final InputStream gpx = GovssTest.class.getResourceAsStream(IMPORT_FILE_PATH);
-
-      final DeviceData deviceData = new DeviceData();
-      final GPXDeviceDataReader deviceDataReader = new GPXDeviceDataReader();
-      final HashMap<Long, TourData> newlyImportedTours = new HashMap<>();
-      final HashMap<Long, TourData> alreadyImportedTours = new HashMap<>();
 
       final GPX_SAX_Handler gpxSaxHandler = new GPX_SAX_Handler(
             deviceDataReader,
