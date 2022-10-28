3d49d1ce3cb2fb1ba1a10ee91dce779e383eef2a
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-10-28 15:25:38 +0000

    misc

--- bundles/net.tourbook/src/net/tourbook/ui/views/tourMarker/TourMarkerView.java
+++ bundles/net.tourbook/src/net/tourbook/ui/views/tourMarker/TourMarkerView.java
@@ -146,7 +146,7 @@ public class TourMarkerView extends ViewPart implements ITourProvider, ITourView
 
    private Menu      _tableContextMenu;
 
-   class MarkerViewerContentProvider implements IStructuredContentProvider {
+   private class MarkerViewerContentProvider implements IStructuredContentProvider {
 
       @Override
       public void dispose() {}
@@ -203,7 +203,7 @@ public class TourMarkerView extends ViewPart implements ITourProvider, ITourView
       }
    }
 
-   public class TableContextMenuProvider implements IContextMenuProvider {
+   private class TableContextMenuProvider implements IContextMenuProvider {
 
       @Override
       public void disposeContextMenu() {
