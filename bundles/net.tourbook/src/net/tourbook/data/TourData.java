46f3b47cc0971be4d4335e590772c1501412b091
FJBDev <25235665+FJBDev@users.noreply.github.com>
2026-09-07 21:21:14 -0600

    wip

--- bundles/net.tourbook/src/net/tourbook/data/TourData.java
+++ bundles/net.tourbook/src/net/tourbook/data/TourData.java
@@ -12020,6 +12020,7 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Serializa
 
       if (hasGeoData() == false) {
          // the time zone needs a geo position
+         //todo fb ca se passe ici
          return 0;
       }
 
