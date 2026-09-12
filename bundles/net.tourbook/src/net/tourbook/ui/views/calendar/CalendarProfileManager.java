8fa281eecff97c4807fd3eef5c026cfc5cf20e33
FJBDev <25235665+FJBDev@users.noreply.github.com>
2026-09-12 11:13:00 -0600

    wip

--- bundles/net.tourbook/src/net/tourbook/ui/views/calendar/CalendarProfileManager.java
+++ bundles/net.tourbook/src/net/tourbook/ui/views/calendar/CalendarProfileManager.java
@@ -196,7 +196,7 @@ class CalendarProfileManager {
    static final RGB               DEFAULT_ALTERNATE_MONTH_RGB            = new RGB(60, 60, 60);
    static final RGB               DEFAULT_ALTERNATE_MONTH2_RGB           = new RGB(80, 80, 80);
    static final RGB               DEFAULT_CALENDAR_BACKGROUND_RGB        = new RGB(40, 40, 40);
-   static final RGB               DEFAULT_CALENDAR_FOREGROUND_RGB    = new RGB(200, 200, 200);
+   static final RGB               DEFAULT_CALENDAR_FOREGROUND_RGB        = new RGB(200, 200, 200);
    static final RGB               DEFAULT_DAY_HOVERED_RGB                = new RGB(255, 255, 255);
    static final RGB               DEFAULT_DAY_SELECTED_RGB               = new RGB(128, 128, 128);
    static final RGB               DEFAULT_DAY_TODAY_RGB                  = new RGB(255, 255, 0);
